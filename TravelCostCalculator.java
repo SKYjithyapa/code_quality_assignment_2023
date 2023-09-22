a0e91855032bd09c246e57968ecefec6430e9bc0
SKYjithyapa <125755221+SKYjithyapa@users.noreply.github.com>
2023-09-22 10:55:54 +0000

    EG_4307

--- TravelCostCalculator.java
+++ TravelCostCalculator.java
@@ -1,3 +1,6 @@
+// EG/2020/4307
+// Yaparathna Y.M.S.K
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,50 +9,50 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TravelCostCalculator {
-    static Map<String, Double> a = new HashMap<>();
-    static Map<String, Double> b = new HashMap<>();
-    static Map<String, Double> c = new HashMap<>();
+    static Map<String, Double> hashMap_a = new HashMap<>();
+    static Map<String, Double> hashMap_b = new HashMap<>();
+    static Map<String, Double> hashMap_c = new HashMap<>();
 
-    static void l1(String file) throws IOException {
+    static void functionHashMap_a(String file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String i; 
         while ((i = reader.readLine()) != null) {
-            String[] p = i.split(",");
-            a.put(p[0].toUpperCase(), Double.parseDouble(p[1]));
+            String[] point = i.split(",");
+            hashMap_a.put(point[0].toUpperCase(), Double.parseDouble(point[1]));
         }
     }
 
-    static void l2(String file) throws IOException {
+    static void functionHashMap_b(String file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String i;
         while ((i = reader.readLine()) != null) {
-            String[] p = i.split(",");
-            b.put(p[0].toUpperCase(), Double.parseDouble(p[1]));
+            String[] point = i.split(",");
+            hashMap_b.put(point[0].toUpperCase(), Double.parseDouble(point[1]));
         }
     }
 
-    static void l3(String file) throws IOException {
+    static void functionHashMap_c(String file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String i;
         while ((i = reader.readLine()) != null) {
-            String[] p = i.split(",");
-            c.put(p[0].toUpperCase(), Double.parseDouble(p[1]));
+            String[] point = i.split(",");
+            hashMap_c.put(point[0].toUpperCase(), Double.parseDouble(point[1]));
         }
     }
 
     public static void main(String[] args) {
         try {
-            l1("data/hotel_rates.csv");
-            l2("data/exchange_rates.csv");
-            l3("data/flight_costs.csv");
+            functionHashMap_a("data/hotel_rates.csv");
+            functionHashMap_b("data/exchange_rates.csv");
+            functionHashMap_c("data/flight_costs.csv");
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
             System.out.print("Enter your destination: ");
             String destination = reader.readLine().toUpperCase();
 
-            double flight_cost = c.getOrDefault(destination, 0.0);
-            double hotel_cost = a.getOrDefault(destination, 0.0);
+            double flight_cost = hashMap_c.getOrDefault(destination, 0.0);
+            double hotel_cost = hashMap_a.getOrDefault(destination, 0.0);
 
             System.out.print("Enter your stay duration in days: ");
             int stay_duration = Integer.parseInt(reader.readLine());
@@ -61,15 +64,15 @@ public class TravelCostCalculator {
             System.out.printf("Hotel cost (%d days): USD %.2f\n", stay_duration, hotel_cost);
             System.out.printf("Total: USD %.2f\n", total_cost_usd);
 
-            String[] available_currencies = b.keySet().toArray(new String[0]);
+            String[] available_currencies = hashMap_b.keySet().toArray(new String[0]);
             System.out.print("Select your currency for final price estimation(" + String.join(", ", available_currencies) + "): ");
             String selected_currency = reader.readLine();
 
-            double final_price_local_currency = total_cost_usd * b.get(selected_currency);
+            double final_price_local_currency = total_cost_usd * hashMap_b.get(selected_currency);
 
             System.out.printf("Total in %s: %.2f\n", selected_currency, final_price_local_currency);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
 }
